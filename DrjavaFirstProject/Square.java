14477cfeebfbfabf028f8718762a6ec077f94beb
hedravBands <hr19ut@brocku.ca>
2020-09-18 10:34:34 -0400

    turning turtle

--- DrjavaFirstProject/Square.java
+++ DrjavaFirstProject/Square.java
@@ -1,6 +1,26 @@
 package DrjavaFirstProject;
+import Media.*;
+import static java.lang.Math.*;
 public class Square{
-public static void main(String[] args){
-System.out.println("Hello BrockU !");
-}
-}
+  private TurtleDisplayer display;
+  private Turtle yertle;
+  public Square(){
+    display = new TurtleDisplayer();
+    yertle = new Turtle();
+    display.placeTurtle(yertle);
+    yertle.penDown();
+    yertle.forward(40);
+    yertle.right(PI/2);
+    yertle.forward(40);
+    yertle.right(PI/2);
+    yertle.forward(40);
+    yertle.right(PI/2);
+    yertle.forward(40);
+    yertle.right(PI/2);
+    yertle.penUp();
+    display.close();
+  }
+  public static void main(String[] args){
+    Square s = new Square();
+  }
+} 
