67972fd7e2b5df81a28d10c92b988f9fe604c3cd
hedravBands <hr19ut@brocku.ca>
2020-10-08 15:07:43 -0400

    comments and descriptions added

--- Assign_1/Assign_1_A/GreekKey.java
+++ Assign_1/Assign_1_A/GreekKey.java
@@ -1,20 +1,47 @@
 package Assign_1_A;
 
-import Media.*;
-import static java.lang.Math.*;
-import static java.awt.Color.*;
+import Media.*;                  // for Turtle and TurtleDisplayer
+import static Media.Turtle.*;    // for Turtle speeds
+import static java.lang.Math.*;  // for Math constants and functions
+import static java.awt.Color.*;  // for Color constants
+
+
+/** This class is a program to draw a pattern called Greek Key using different turns
+  * 
+  * @author Heduin R. B. de Morais (Brock ID 6967483, Campus ID hr19ut, Lab#09) 
+  * 
+  * @version 1.0 (Oct 2020)
+  *
+  * new concepts: constructor function method    */
+
 
 public class GreekKey {
-  private TurtleDisplayer display;
-  private Turtle donatello;
+  private TurtleDisplayer display;    // display to draw on
+  private Turtle donatello;           // (ninja) turtle to do the drawing
 
+  
+  /** The constructor draws a Greek Key pattern using repeated method calls for moving and turning.      */
+  
   public GreekKey ( ) {
     
     display = new TurtleDisplayer();
-    donatello = new Turtle();
-    display.placeTurtle(donatello);
+    donatello = new Turtle(FAST);  // for faster drawing
+    display.placeTurtle(donatello);  // @var named after the ninja turtle
     
+    drawCell(); 
+    
+    display.close();
+    
+};  // constructor
+  
+
+  /** This method draws the internal pattern.
+    * It leaves the the turtle 16 positions forward the original position
+    * It does not change the direction of the turtle                                */
+                                                                          
+  private void drawCell () {
     donatello.penDown();
+    
     donatello.forward(2);
     donatello.left(PI/2);
     donatello.forward(12);
@@ -32,11 +59,15 @@ public class GreekKey {
     donatello.forward(8);
     donatello.left(PI/2);
     donatello.forward(10);
+    
     donatello.penUp();
     
-    display.close();
-};
   
+  } // drawCell
+  
+  
+  
+  //@var splinter is due to ninja turtles' master name
 public static void main ( String[] args ) { GreekKey splinter = new GreekKey(); };
 
-}
+}  // GreekKey
--- Assign_1/Assign_1_C/Frame.java
+++ Assign_1/Assign_1_C/Frame.java
@@ -1,69 +1,58 @@
 package Assign_1_C;
 
-import Media.*;
-import static java.lang.Math.*;
-import static java.awt.Color.*;
-import static Media.Turtle.*;
+import Media.*;                  // for Turtle and TurtleDisplayer
+import static Media.Turtle.*;    // for Turtle speeds
+import static java.lang.Math.*;  // for Math constants and functions
+import static java.awt.Color.*;  // for Color constants
+
+
+/** This class is a program to draw a sequence of Greek Key patterns by calling repeatly the function drawCell
+  * created in the Assign_1_A and Assign_1_B
+  * 
+  * @author Heduin R. B. de Morais (Brock ID 6967483, Campus ID hr19ut, Lab#09) 
+  * 
+  * @version 1.0 (Oct 2020)
+  *
+  * new concepts: for loops, initial positioning, calling different functions    
+  * 
+  * @param nKey number of individual cells to form a Border
+  * @param nSide number of sides to form the Frame                      */
 
 public class Frame {
   private TurtleDisplayer display;
-  private Turtle raphael; //draws 4 sides of 8 squares each
-  
+  private Turtle raphael; // (ninja) turtle needs to draw 4 sides of 8 greekKeys each
+  private int nKey;
+  private int nSide;
 
-  public Frame ( ) {
+  public Frame (int nKey, int nSide ) {
     
     display = new TurtleDisplayer();
-    raphael = new Turtle(FAST); //parameter FAST is passed to constructor
+    raphael = new Turtle(1); // int = 1, rather than CONSTANT FAST for a much faster speed
     display.placeTurtle(raphael);
+    int nS = nSide;
+    int nK = nKey;
     
 // initial position to start drawing;
     raphael.left(PI); //could be .right(Pi) due to angle Pi, now facing LEFT
-    raphael.forward(16*4);  // 4 greekKeys with side 16 each to LEFT
+    raphael.forward(16*nKey/2);  // half-side with side 16 each to LEFT
     raphael.right(PI/2); // now facing UP
-    raphael.forward(16*4);  // moving 4 greekKeys UPWARDS
+    raphael.forward(16*nKey/2);  // moving half-side greekKeys UPWARDS
     raphael.right(PI/2); //turning half circle, now facing RIGHT
     
-
-//turtle is positioned on TOP LEFT CORNER facing RIGHT and ready to draw
-    raphael.penDown();
-    
-    // 4 sides need to be drawn, var i as outer counter
-    for(int i = 0; i < 4; i++) {
-    
-    //drawing of 8 greek keys, a.k.a. side, var j as nested counter
-      for(int j = 0; j < 8; j++) {  
-        
-        raphael.forward(2);
-        raphael.left(PI/2);
-        raphael.forward(12);
-        raphael.right(PI/2);
-        raphael.forward(12);
-        raphael.right(PI/2);
-        raphael.forward(8);
-        raphael.right(PI/2);
-        raphael.forward(4);
-        raphael.right(PI/2);
-        raphael.forward(4);
-        raphael.left(PI/2);
-        raphael.forward(4);
-        raphael.left(PI/2);
-        raphael.forward(8);
-        raphael.left(PI/2);
-        raphael.forward(10);
-        
-      } //end of nested loop 
-
-//let's turn the (ninja) turtle 90 degrees to the RIGHT
+// drawing each nSide sides of the figure, and turn     
+    for (int i = 0; i < nSide; i++ ) {     
+      
+      drawBorder(nKey);
       raphael.right(PI/2);
       
-    } // end of outter loop
+      
+               
+    }  // for nSide   
     
-// now 4 sides are drawn, pen must be set UP to avoid further marks on display        
-    raphael.penUp();
     
     
-/* 
- * before closing display, the forth ninja turtle is sad for not being part of this assignment,
+  /* 
+ * before closing display, the 4th. ninja turtle is sad for not being part of this assignment,
  * so let's mention his creation here and practice the use of block comments:
  * 
  * private Turtle leonardo;
@@ -71,16 +60,69 @@ public class Frame {
  * leonardo = new Turtle(FAST);
  * display.placeTurtle(leonardo);
  * 
- * leonardo.left(4*2*PI); // performing an impressive victory turn!
+ * leonardo.left(10*2*PI); // performing an impressive victory turn! =)
  * 
  * leonardo.penDown(); // practicing calling some methods
  * leonardo.penUp(); // leonardo is now happy eventhough there's no Assign_1_D
  * 
 */
-    display.close();
-};
-  
-//var pipeLine is considering the full pipe where ninja turtles live
-public static void main ( String[] args ) { Frame pipeLine = new Frame(); };
 
-}
+    display.close();
+     
+};  // constructor
+  
+  /** This method draws each border or side of the big pattern.
+    * It leaves the the turtle 16*nKey positions forward the original position
+    * It does not change the direction of the turtle        */
+  
+  private void drawBorder (int nKey) {    
+        
+    for (int j = 0; j < nKey; j++){
+    
+      drawCell();
+    
+    }  // for nKey    
+    
+   
+    
+};  // drawBorder
+
+  
+  /** This method draws the internal pattern cell.
+    * It leaves the the turtle 16 positions forward the original position
+    * It does not change the direction of the turtle        */
+                                                                          
+  private void drawCell () {
+    raphael.penDown();
+    
+    raphael.forward(2);
+    raphael.left(PI/2);
+    raphael.forward(12);
+    raphael.right(PI/2);
+    raphael.forward(12);
+    raphael.right(PI/2);
+    raphael.forward(8);
+    raphael.right(PI/2);
+    raphael.forward(4);
+    raphael.right(PI/2);
+    raphael.forward(4);
+    raphael.left(PI/2);
+    raphael.forward(4);
+    raphael.left(PI/2);
+    raphael.forward(8);
+    raphael.left(PI/2);
+    raphael.forward(10);
+    
+    raphael.penUp();
+    
+  
+  }  // drawCell
+  
+  
+  
+  
+// @var pipeLine is considering the full pipe where ninja turtles live with 8 greekKeys, and 4 sides as arguments
+public static void main ( String[] args ) { Frame pipeLine = new Frame(8, 4); };
+
+}  // Frame
+
